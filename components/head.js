71a05ea87b7d9000b45442cca658f9c1208d5d07
Vicky Kumar <vickykumar@Vickys-MacBook-Air.local>
2023-07-30 12:10:00 +0530

    adstxt added

--- components/head.js
+++ components/head.js
@@ -1,8 +1,6 @@
 function head(){
     return `
     <link rel="icon" href="./images/vicky-profile-trans.png" type="image/x-icon">
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3359301633531008"
-     crossorigin="anonymous"></script>
     `
 }
 export default head;
